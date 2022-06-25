109cde323609022c4ea2bdc2ee597bb7db52a136
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-25 12:00:56 +1000

    updated project structure again

---
+++ packages/atom/index.ts
@@ -0,0 +1,3 @@
+export {AtomFactory} from "./src/factory.interface";
+export {AtomFactoryImpl} from "./src/factory";
+export {Atom, LeafAtom, DerivedAtom} from "./src/atom.interface";
--- packages/atom/src/index.ts
+++
@@ -1,3 +0,0 @@
-export {AtomFactory} from "./factory.interface";
-export {AtomFactoryImpl} from "./factory";
-export {Atom, LeafAtom, DerivedAtom} from "./atom.interface";
