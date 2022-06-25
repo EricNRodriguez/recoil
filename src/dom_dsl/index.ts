5c4ac301c02e5e5d5bb125fc690f1bfb1453773f
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-25 18:39:27 +1000

    export runapp

--- src/dom_dsl/index.ts
+++ src/dom_dsl/index.ts
@@ -5,3 +5,4 @@ export {h1, h2, h3, h4, h5, h6} from "./src/header";
 export {foreach} from "./src/foreach";
 export {ifElse} from "./src/if";
 export {} from "./src/element_ext";
+export {runApp} from "./src/run_app"
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export {buildFactory, AtomFactory, Atom, LeafAtom, DerivedAtom, isAtom} from "./atom";
-export {h1, h2, h3, h4, h5, h6, ifElse, foreach, div, p, t} from "./dom_dsl";
+export {h1, h2, h3, h4, h5, h6, ifElse, foreach, div, p, t, runApp} from "./dom_dsl";
