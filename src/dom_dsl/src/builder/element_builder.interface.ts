99d4a02d4b618735db1b958db72b46e69cb56f59
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-26 21:47:35 +1000

    added dyanmic inputs to element builder interface

--- src/dom_dsl/src/builder/element_builder.interface.ts
+++ src/dom_dsl/src/builder/element_builder.interface.ts
@@ -1,9 +1,11 @@
 import {NodeBuilder} from "./node_builder.interface";
 import {Runnable} from "../../../atom/src/util.interface";
+import {Atom} from "../../../atom";
+import {Supplier} from "../util.interface";
 
 export interface ElementBuilder extends NodeBuilder{
     build(): Node;
-    withClass(className: string): ElementBuilder;
+    withClass(className: string | Atom<string> | Supplier<string>): ElementBuilder;
     withId(id: string): ElementBuilder;
     withClickHandler(handler: Runnable): ElementBuilder;
     withChildren(...children: Element[]): ElementBuilder;
