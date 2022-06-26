2d2a70ddb1ca8aacf2eb6adcc546582cb0b288f5
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-26 21:28:30 +1000

    adapt runapp

--- src/dom_dsl/src/run_app.ts
+++ src/dom_dsl/src/run_app.ts
@@ -1,3 +1,6 @@
-export const runApp = (anchor: Element, app: Element): void => {
-    anchor.replaceChildren(app);
+import {ElementBuilder} from "./builder/element_builder.interface";
+import {unwrapNodesFromBuilder} from "./builder/builder_util";
+
+export const runApp = (anchor: Element, app: Element | ElementBuilder): void => {
+    anchor.replaceChildren(unwrapNodesFromBuilder<Element>(app));
 };
