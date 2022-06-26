90a6547def5febcf601e327156b29d51457b088a
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-26 20:54:40 +1000

    withClickHandler wither

--- src/dom_dsl/src/element_ext.ts
+++ src/dom_dsl/src/element_ext.ts
@@ -1,6 +1,11 @@
+interface ClickHandler {
+    (): void;
+}
+
 declare interface Element {
     withClassName(className: string): Element;
     withId(id: string): Element;
+    withClickHandler(callback: ClickHandler): Element;
 }
 
 Element.prototype.withClassName = function(this: Element, className: string): Element {
@@ -10,5 +15,13 @@ Element.prototype.withClassName = function(this: Element, className: string): El
 
 Element.prototype.withId = function (this: Element, id: string): Element {
     this.id = id;
+    return this;
+};
+
+Element.prototype.withClickHandler = function (this: Element, callback: ClickHandler): Element {
+    if (!(this instanceof HTMLElement)) {
+        throw new Error("withClickHandler called on element that is not clickable");
+    }
+    (this as HTMLElement).addEventListener("click", callback);
     return this;
 };
