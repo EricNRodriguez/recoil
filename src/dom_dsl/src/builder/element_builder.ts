b0c032b89010b8ff5b64f294451cad7cb25450a6
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-26 21:45:15 +1000

    allow dynamic classnames

--- src/dom_dsl/src/builder/element_builder.ts
+++ src/dom_dsl/src/builder/element_builder.ts
@@ -1,16 +1,32 @@
 import {NodeBuilder} from "./node_builder.interface";
 import {Runnable} from "../../../atom/src/util.interface";
 import {ElementBuilder} from "./element_builder.interface";
+import {Supplier} from "../util.interface";
+import {Atom, buildFactory, isAtom} from "../../../atom";
+import {Reference} from "../../../atom/src/factory.interface";
 
 export class ElementBuilderImpl implements ElementBuilder {
     private readonly element: HTMLElement;
+    private readonly effectRefs: WeakMap<HTMLElement, Reference> = new WeakMap();
 
     constructor(tag: string) {
         this.element = document.createElement(tag);
     }
 
-    public withClass(className: string): ElementBuilder {
-        this.element.className = `${this.element.className} ${className}`;
+    public withClass(className: string | Atom<string> | Supplier<string>): ElementBuilder {
+        if (typeof className === "string") {
+            this.element.className = `${this.element.className} ${className}`;
+        } else {
+            const prevClassName = this.element.className;
+            const effectRef: Reference = buildFactory().createEffect((): void => {
+                const newClassName: string = isAtom(className) ?
+                    (className as Atom<any>).get() :
+                    (className as Supplier<string>)();
+
+                this.element.className = `${prevClassName} ${newClassName}`;
+            })
+            this.effectRefs.set(this.element, effectRef);
+        }
         return this;
     }
 
