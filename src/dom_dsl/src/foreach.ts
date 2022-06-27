053cb236b813320360a599522c847fef9a080a19
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-27 20:19:27 +1000

    hack tmp fix for foreach

--- src/dom_dsl/src/foreach.ts
+++ src/dom_dsl/src/foreach.ts
@@ -2,30 +2,41 @@ import {Function, Supplier} from "./util.interface";
 import {AtomFactory, buildFactory} from "../../atom";
 import {bindScope, removeAllChildren} from "./dom_utils";
 import {NodeBuilder} from "./builder/node_builder.interface";
-import {unwrapNodesFromBuilder} from "./builder/builder_util";
 import {Reference} from "../../atom/src/factory.interface";
 
 const atomFactory: AtomFactory = buildFactory();
 
 export const foreach = <T extends Object>(getItems: Supplier<T[]>, buildElement: Function<T, Node | NodeBuilder>): Node => {
-    const elementCache: WeakMap<T, Node> = new WeakMap();
+    const elementCache: WeakMap<T, Node | NodeBuilder> = new WeakMap();
 
-    const commentAnchor: Comment = document.createComment("foreach-anchor");
+    const anchor: HTMLElement = document.createElement("div");
+    anchor.style.setProperty("display", "contents");
+
     const effectRef: Reference = atomFactory.createEffect((): void => {
-        removeAllChildren(commentAnchor);
+        removeAllChildren(anchor);
 
         getItems().forEach((item: T): void => {
-            if (!elementCache.has(item)) {
-                const builtItem: Node = unwrapNodesFromBuilder<Node>(buildElement(item)) as Node;
-                elementCache.set(item, builtItem);
-            }
-
-            commentAnchor.appendChild(
-                elementCache.get(item)!,
+            let builtItem: Node | NodeBuilder | undefined = buildElement(item);
+            anchor.appendChild(
+                "build" in builtItem ? (builtItem as NodeBuilder).build() : builtItem,
             );
+
+            // if (!elementCache.has(item)) {
+            //     const builtItem: Node = unwrapNodesFromBuilder<Node>(buildElement(item)) as Node;
+            //     // elementCache.set(item, builtItem);
+            // }
+            //
+            // let builtItem: Node | NodeBuilder | undefined = elementCache.get(item);
+            // if (builtItem === undefined) {
+            //     return;
+            // }
+            // anchor.appendChild(
+            //     "build" in builtItem ? (builtItem as NodeBuilder).build() : builtItem,
+            // );
         });
     });
 
-    bindScope(commentAnchor, effectRef);
-    return commentAnchor;
+    bindScope(anchor, effectRef);
+    bindScope(anchor, elementCache);
+    return anchor;
 };
