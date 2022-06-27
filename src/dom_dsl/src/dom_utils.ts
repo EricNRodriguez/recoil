9c411441bc985c7652191a5fc813a99a0a93e0ae
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-27 19:09:08 +1000

    improve scope context doc

--- src/dom_dsl/src/dom_utils.ts
+++ src/dom_dsl/src/dom_utils.ts
@@ -16,13 +16,16 @@ export const replaceChildren = (node: Element, ...children: (Node | undefined | 
     );
 };
 
-
-// A registry used to keep references in memory for the lifetime of the node,
+// A registry used to keep references in memory for the lifetime of the node.
 //
 // This is intended to be used to bind the scope of side effects to the DOM nodes
 // that they operate on. Since the atom DAG stores references weakly to prevent
 // difficult to find memory leaks, effects will not be referred to strongly
 // by the atom lib, with it being the responsibility of the callers to scope it.
+//
+// One thought would be to do some fancy meta programming to figure out this at runtime,
+// similar to dependency detection in the atom framework but via proxies... sounds yuck and
+// over the top, this is fine for now.
 //
 // This introduces a second issue: Our effects refer strongly to our DOM nodes,
 // so won't they cause a circular reference and never be collected by the weak
