a695d45f69804c1baa77602646e9f437a2c7874b
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-27 19:15:56 +1000

    fix type error

--- src/dom_dsl/src/run_app.ts
+++ src/dom_dsl/src/run_app.ts
@@ -2,5 +2,5 @@ import {ElementBuilder} from "./builder/element_builder.interface";
 import {unwrapNodesFromBuilder} from "./builder/builder_util";
 
 export const runApp = (anchor: Element, app: Element | ElementBuilder): void => {
-    anchor.replaceChildren(unwrapNodesFromBuilder<Element>(app));
+    anchor.replaceChildren(unwrapNodesFromBuilder<Element>(app) as Node);
 };
