7ca13d1bb1f43c9f6e4df45b0f849411efa7fb72
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-06-28 17:54:01 +1000

    export indexeditem interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export {buildFactory, AtomFactory, Atom, LeafAtom, DerivedAtom, isAtom} from "./atom";
-export {h1, h2, h3, h4, h5, h6, hr, br, ifElse, foreach, div, p, t, runApp, NodeBuilder, ElementBuilder} from "./dom_dsl";
+export {h1, h2, h3, h4, h5, h6, hr, br, ifElse, foreach, div, p, t, runApp, NodeBuilder, ElementBuilder, IndexedItem} from "./dom_dsl";
