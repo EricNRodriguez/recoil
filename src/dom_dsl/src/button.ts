44dc444b8d8c33a07ae0f0fcac9aa3a858bb5264
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-03 20:07:48 +1000

    provide hook to inject onclick in button dsl helper

--- src/dom_dsl/src/button.ts
+++ src/dom_dsl/src/button.ts
@@ -1,10 +1,17 @@
 import {ElementBuilder} from "./builder/element_builder.interface";
 import {ElementBuilderImpl} from "./builder/element_builder";
+import {Consumer} from "../../atom/src/util.interface";
 
 export type ButtonContent = Text | string;
 
-export const button = (content: ButtonContent): ElementBuilder => {
+export type ButtonArgs = {
+    content: ButtonContent,
+    onClick: Consumer<MouseEvent>,
+};
+
+export const button = (args: ButtonArgs): ElementBuilder => {
     return new ElementBuilderImpl("button")
         .withAttribute("type", "button")
-        .withChildren(content);
+        .withChildren(args.content)
+        .withClickHandler(args.onClick);
 };
