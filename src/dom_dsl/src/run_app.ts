799b06607607d2064919b8addea407d0e8b9333e
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-12 19:06:56 +1000

    mount app in run app

--- src/dom_dsl/src/run_app.ts
+++ src/dom_dsl/src/run_app.ts
@@ -2,11 +2,12 @@ import {unwrapVNode} from "./vdom/vdom_util";
 import {VNode} from "./vdom/virtual_node.interface";
 import {HtmlVNode} from "./vdom/virtual_node";
 import {replaceChildren} from "./util/dom_utils";
+import {HtmlVElement} from "./vdom/virtual_element";
 
-export const runApp = (anchor: Element, app: HtmlVNode): void => {
-    app.mount();
+export const runApp = (anchor: Element, app: HtmlVElement): void => {
     replaceChildren(
         anchor,
         unwrapVNode(app)
     );
+    app.mount();
 };
--- src/dom_dsl/src/vdom/virtual_node_base.ts
+++ src/dom_dsl/src/vdom/virtual_node_base.ts
@@ -5,12 +5,19 @@ import {bindScope} from "../util/dom_utils";
 export abstract class VNodeBase<A, B extends VNodeBase<A,B>> implements VNode<A, B> {
     private readonly node: A;
     private readonly rootEffects: Set<SideEffectRef> = new Set<SideEffectRef>();
+    private isMounted: boolean = false;
 
     constructor(node: A) {
         this.node = node;
     }
 
     public registerEffect(effectRef: SideEffectRef): VNodeBase<A, B> {
+        if (this.isMounted) {
+            effectRef.deactivate();
+        } else {
+            effectRef.activate();
+        }
+
         this.rootEffects.add(effectRef);
         bindScope(
             this.node,
@@ -20,11 +27,15 @@ export abstract class VNodeBase<A, B extends VNodeBase<A,B>> implements VNode<A,
     }
 
     public mount(): VNodeBase<A, B> {
+        this.isMounted = true;
+
         this.activateEffects();
         return this;
     }
 
     public unmount(): VNodeBase<A, B> {
+        this.isMounted = false;
+
         this.deactivateEffects();
         return this;
     }
