fdad30d250ea0d59c77fd7fbcc1c117e5f7ade35
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-13 19:08:58 +1000

    remove indirection in foreach

--- src/dom_dsl/src/control/foreach.ts
+++ src/dom_dsl/src/control/foreach.ts
@@ -4,7 +4,6 @@ import {IndexedItem} from "../indexed_item.interface";
 import {getItem, getKey} from "../indexed_item_lense";
 import {MaybeNodeOrVNode} from "../node.interface";
 import {HtmlVNode} from "../vdom/virtual_node";
-import {HtmlVElement} from "../vdom/virtual_element";
 
 export const foreach = <T extends Object>(
     getItems: Supplier<IndexedItem<T>[]>,
@@ -12,46 +11,35 @@ export const foreach = <T extends Object>(
 ): HtmlVNode => {
     const anchor = frag();
 
-    anchor.registerSideEffect(
-        buildUpdateAnchorSideEffect(anchor, getItems, buildElement)
-    );
-
-    return anchor;
-};
-
-const buildUpdateAnchorSideEffect = <T>(
-    anchor: HtmlVElement,
-    getItems: Supplier<IndexedItem<T>[]>,
-    buildElement: Function<T, MaybeNodeOrVNode>
-): () => void => {
     let currentItemOrder: string[] = [];
     let currentItemIndex: Map<string, MaybeNodeOrVNode> = new Map();
+    anchor.registerSideEffect((): void => {
+            const newItems: IndexedItem<T>[] = getItems();
+            const newItemOrder: string[] = newItems.map(getKey);
+            const newItemNodesIndex: Map<string, MaybeNodeOrVNode> = new Map(
+                newItems.map((item: IndexedItem<T>): [string, MaybeNodeOrVNode] => [
+                    getKey(item),
+                    currentItemIndex.get(getKey(item)) ?? buildElement(getItem(item)),
+                ]),
+            );
 
-    return (): void => {
-        const newItems: IndexedItem<T>[] = getItems();
-        const newItemOrder: string[] = newItems.map(getKey);
-        const newItemNodesIndex: Map<string, MaybeNodeOrVNode> = new Map(
-            newItems.map((item: IndexedItem<T>): [string, MaybeNodeOrVNode] => [
-                getKey(item),
-                currentItemIndex.get(getKey(item)) ?? buildElement(getItem(item)),
-            ]),
-        );
-
-        let firstNonEqualIndex: number = 0;
-        while (firstNonEqualIndex < currentItemOrder.length &&
-               firstNonEqualIndex < newItems.length &&
+            let firstNonEqualIndex: number = 0;
+            while (firstNonEqualIndex < currentItemOrder.length &&
+            firstNonEqualIndex < newItems.length &&
             currentItemOrder[firstNonEqualIndex] === newItemOrder[firstNonEqualIndex]) {
-            ++firstNonEqualIndex;
-        }
+                ++firstNonEqualIndex;
+            }
 
-        anchor.deleteChildren(firstNonEqualIndex);
-        anchor.appendChildren(
-            newItemOrder
-                .slice(firstNonEqualIndex)
-                .map(key => newItemNodesIndex.get(key))
-        );
+            anchor.deleteChildren(firstNonEqualIndex);
+            anchor.appendChildren(
+                newItemOrder
+                    .slice(firstNonEqualIndex)
+                    .map(key => newItemNodesIndex.get(key))
+            );
 
-        currentItemOrder = newItemOrder;
-        currentItemIndex = newItemNodesIndex;
-    };
+            currentItemOrder = newItemOrder;
+            currentItemIndex = newItemNodesIndex;
+    });
+
+    return anchor;
 };
