913515cc9dbcfb9abec49037667a7624274824a6
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-16 20:55:02 +1000

    simplify import path

--- packages/atom/src/api.ts
+++ packages/atom/src/api.ts
@@ -1,7 +1,7 @@
 import { LeafAtom, DerivedAtom, SideEffectRef } from "./atom.interface";
 import { LeafAtomImpl, DerivedAtomImpl } from "./atom";
 import { Atom } from "./atom.interface";
-import { Consumer, Producer, Runnable } from "../../util/index";
+import { Consumer, Producer, Runnable } from "../../util";
 
 export type FetchStateOptionalArgs = {
   autoScope?: boolean;
