b22dd38a46b899179594d3eaa14c7c1d66759f5b
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-16 20:16:00 +1000

    update text input to be two-way binded

--- packages/dom_dsl/src/input/text_input.ts
+++ packages/dom_dsl/src/input/text_input.ts
@@ -1,13 +1,17 @@
 import { HtmlVElement } from "../vdom/virtual_element";
-import { Consumer } from "../../../util";
+import {createScope, LeafAtom, runEffect} from "../../../atom";
 
-export type TextInputArgs = {
-  onInput: Consumer<string | null>;
-};
-
-export const textInput = (args: TextInputArgs): HtmlVElement => {
+export const textInput = createScope((text: LeafAtom<string>): HtmlVElement => {
   const inputElement = document.createElement("input");
-  return new HtmlVElement(inputElement)
-    .setAttribute("type", "text")
-    .addEventHandler("input", () => args.onInput(inputElement.value));
-};
+  
+  const element = new HtmlVElement(inputElement)
+    .setAttribute("type", "text");
+
+  element.addEventHandler('input', () => text.set(inputElement.value));
+
+  runEffect({effect: (): void => {
+      inputElement.value = text.get();
+  }});
+
+  return element;
+});
