967b5da7ecb8d112145340da3e991d01db8ed5ad
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-17 12:41:17 +1000

    register run effect decorator

--- packages/dom_dsl/src/component/create_component.ts
+++ packages/dom_dsl/src/component/create_component.ts
@@ -1,9 +1,29 @@
 import {HtmlVNode} from "../vdom/virtual_node";
+import {
+    deregisterRunEffectDecorator,
+    registerRunEffectDecorator,
+    RunEffectDecorator,
+    RunEffectSignature,
+    SideEffectRef
+} from "../../../atom";
+import {Runnable} from "../../../util";
 
 export type ComponentBuilder<T extends HtmlVNode> = (...args: any[]) => T;
 
 export const createComponent = <T extends HtmlVNode>(fn: ComponentBuilder<T>): ComponentBuilder<T> => {
+    const collectCreatedEffects: RunEffectDecorator = (runEffect: RunEffectSignature): RunEffectSignature => {
+        return (rawEffect: Runnable): SideEffectRef => {
+            return runEffect(rawEffect);
+        };
+    };
+
     return (...args: any[]): T => {
-        return fn(...args);
+        try {
+            registerRunEffectDecorator(collectCreatedEffects);
+            return fn(...args);
+        } finally {
+            deregisterRunEffectDecorator(collectCreatedEffects);
+        }
+
     };
 };
