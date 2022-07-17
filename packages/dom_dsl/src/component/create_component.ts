90a7f89581cc59c0500a02374f8eb0727b6646a8
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-17 16:40:00 +1000

    fix typo in createComponent doc

--- packages/dom_dsl/src/component/create_component.ts
+++ packages/dom_dsl/src/component/create_component.ts
@@ -30,7 +30,7 @@ export type DomBuilder<T extends HtmlVNode> = (...args: any[]) => T;
  * A higher order function that creates components. A component is any stateful Dom builder, i.e. any function
  * that constructs a DOM tree that contains ANY stateful logic.
  *
- * This function provides some 'magic' that automatically binds the lifecycle or any side effects created within
+ * This function provides some 'magic' that automatically binds the lifecycle of any side effects created within
  * the injected builder to the lifecycle of the constructed Dom node. I.e. when it is mounted, they are activated,
  * when it is unmounted, they are deactivated. This is critical for performance, as it ensures that the only eager
  * updates being made to the DOM are visible by the user. A result of this is that side effects are automatically
