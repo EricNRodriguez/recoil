6b40bdffa87386cbafc6039129c4460c70f41dbe
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-17 16:31:57 +1000

    document the createComponent method

--- packages/dom_dsl/src/component/create_component.ts
+++ packages/dom_dsl/src/component/create_component.ts
@@ -21,13 +21,37 @@ class EffectCollector {
   }
 }
 
+/**
+ * A plain old javascript function that returns a HtmlVNode (or subclass of it)
+ */
 export type ComponentBuilder<T extends HtmlVNode> = (...args: any[]) => T;
 
+/**
+ * A higher order function that creates components. A component is any stateful Dom builder, i.e. any function
+ * that constructs a DOM tree that contains ANY stateful logic.
+ *
+ * This function provides some 'magic' that automatically binds the lifecycle or any side effects created within
+ * the injected builder to the lifecycle of the constructed Dom node. I.e. when it is mounted, they are activated,
+ * when it is unmounted, they are deactivated. This is critical for performance, as it ensures that the only eager
+ * updates being made to the DOM are visible by the user. A result of this is that side effects are automatically
+ * kept alive for the lifetime of the DOM tree, which means the references returned by the runEffect factory method
+ * can be safely ignored.
+ *
+ * @param fn The HtmlVNode builder to be wrapped.
+ * @returns The wrapped function
+ */
 export const createComponent = <T extends HtmlVNode>(
   fn: ComponentBuilder<T>
 ): ComponentBuilder<T> => {
   const collector: EffectCollector = new EffectCollector();
 
+
+  /**
+   * A runtime decorator around the runEffect method (provided by the atom package) that collects them for future use.
+   *
+   * @param runEffect The current runEffect method (may or may not be already decorated)
+   * @returns A decoration of the provided runEffect method
+   */
   const collectCreatedEffects: FunctionDecorator<RunEffectSignature> = (
     runEffect: RunEffectSignature
   ): RunEffectSignature => {
