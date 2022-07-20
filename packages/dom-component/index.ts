f9c477cc49ddad2a5b2ac28db5ce759b1e8ed50a
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-20 16:59:34 +1000

    export dom builder

--- packages/dom-component/index.ts
+++ packages/dom-component/index.ts
@@ -1,2 +1,2 @@
-export {createComponent} from "./src/component_factory";
+export {createComponent, DomBuilder} from "./src/component_factory";
 export {onMount, onUnmount, onInitialMount, mountEffect, runMountedEffect} from "./src/mount_hooks";
