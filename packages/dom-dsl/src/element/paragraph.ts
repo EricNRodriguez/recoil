8bee923f55d22658d0831a9fd52a74faf2cb116c
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-20 17:02:00 +1000

    only allow paragraph content to have a single child

--- packages/dom-dsl/src/element/paragraph.ts
+++ packages/dom-dsl/src/element/paragraph.ts
@@ -2,6 +2,6 @@ import { HtmlVElement } from "../../../vdom";
 
 type ParagraphContent = Text | string;
 
-export const p = (...children: ParagraphContent[]): HtmlVElement => {
-  return new HtmlVElement("p").setChildren(...children);
+export const p = (children: ParagraphContent): HtmlVElement => {
+  return new HtmlVElement("p").setChildren(children);
 };
