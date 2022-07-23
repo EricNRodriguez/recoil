306a965bb0f366a8c4548452b6299c3010d23b39
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-23 17:35:10 +1000

    move frag back to unboxed div

--- packages/dom-dsl/src/element/frag.ts
+++ packages/dom-dsl/src/element/frag.ts
@@ -1,9 +1,9 @@
 import { MaybeNodeOrVNode } from "./node.interface";
 import { VNode } from "../../../vdom";
+import {div} from "./div";
 
 export type FragContent = MaybeNodeOrVNode;
 
 export const frag = (...children: FragContent[]): VNode => {
-  const rawElem = document.createDocumentFragment();
-  return new VNode(rawElem).setChildren(...children);
+  return div(...children).setStyle({"display": "content"});
 };
