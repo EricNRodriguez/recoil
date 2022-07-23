857bd80c3cdc1a4cdbffffc7886e9cfdc2796ff7
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-23 16:41:43 +1000

    Revert "fixing circular imports"
    This reverts commit d8e835e259ee7a4856d2fb9b83e5b15b4a8ee06f.

--- packages/vdom/src/virtual_node.ts
+++ packages/vdom/src/virtual_node.ts
@@ -1,23 +1,7 @@
-import {isVNode, VNodeBase} from "./virtual_node_base";
-import {VNode} from "./virtual_node.interface";
-import {nullOrUndefined} from "../../util";
+import { VNodeBase } from "./virtual_node_base";
 
 export class HtmlVNode extends VNodeBase<Node, HtmlVNode> {
   constructor(node: Node) {
     super(node);
   }
 }
-export const wrapInVNode = (
-  node: VNode<any, any> | Node | string | null | undefined
-): VNode<any, any> | null | undefined => {
-  if (nullOrUndefined(node)) {
-    return node as null | undefined;
-  }
-
-  if (isVNode(node)) {
-    return node as VNode<any, any>;
-  } else {
-    return new HtmlVNode(node as Node);
-  }
-};
-
