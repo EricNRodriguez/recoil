83a288add42fa50dd387cf92217fa69a94e5fe78
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-28 19:27:52 +1000

    pull atoms out of dom wrapper - keep it thin

--- packages/dom/src/element.ts
+++ packages/dom/src/element.ts
@@ -1,9 +1,6 @@
 import { BiConsumer, Consumer } from "../../util";
-import { IAtom, isAtom } from "../../atom";
 import { BaseWNode } from "./node";
 export type ElementStyle = { [key: string]: string };
-
-export type Attribute = string | IAtom<string>;
 
 export abstract class BaseWElement<
   A extends HTMLElement,
@@ -13,29 +10,8 @@ export abstract class BaseWElement<
     super(element);
   }
 
-  public setAttribute(attribute: string, value: Attribute): B {
-    if (isAtom(value)) {
-      return this.setAtomicAttribute(attribute, value as IAtom<string>);
-    } else if (typeof value === "string") {
-      return this.setStaticAttribute(attribute, value);
-    }
-
-    // TODO(ericr): replace with specific fall through error
-    throw new Error("unsupported attribute type");
-  }
-
-  private setStaticAttribute(attribute: string, value: string): B {
+  public setAttribute(attribute: string, value: string): B {
     this.unwrap().setAttribute(attribute, value);
-    return this as unknown as B;
-  }
-
-  private setAtomicAttribute(attribute: string, value: IAtom<string>): B {
-    const ref = value.react((): void => {
-      this.setAttribute(attribute, value.get());
-    });
-
-    this.registerOnMountHook(ref.activate.bind(ref));
-    this.registerOnUnmountHook(ref.deactivate.bind(ref));
     return this as unknown as B;
   }
 
