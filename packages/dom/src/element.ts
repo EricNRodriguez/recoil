b354d1016bf7adcaf6ae8d681893551cb49903a5
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-07-30 23:17:51 +1000

    make WElement implement WNode

--- packages/dom/src/element.ts
+++ packages/dom/src/element.ts
@@ -1,11 +1,11 @@
 import { BiConsumer, Consumer } from "../../util";
-import { BaseWNode } from "./node";
+import {BaseWNode, WNode} from "./node";
 export type ElementStyle = { [key: string]: string };
 
 export abstract class BaseWElement<
   A extends HTMLElement,
   B extends BaseWElement<A, B>
-> extends BaseWNode<A, B> {
+> extends BaseWNode<A, B> implements WNode<A> {
   constructor(element: A) {
     super(element);
   }
