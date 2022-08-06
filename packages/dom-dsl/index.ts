9a04b31503c1c3a22cb4ce489effb4cea09a30fe
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-06 14:50:42 +1000

    fmt

--- packages/dom-dsl/index.ts
+++ packages/dom-dsl/index.ts
@@ -2,9 +2,9 @@ export { foreach } from "./src/control/foreach";
 export { ifElse } from "./src/control/if";
 export { match } from "./src/control/match";
 export { div } from "./src/element";
-export { p } from "./src/element"
-export { t } from "./src/element"
-export { h1, h2, h3, h4, h5, h6 } from "./src/element"
+export { p } from "./src/element";
+export { t } from "./src/element";
+export { h1, h2, h3, h4, h5, h6 } from "./src/element";
 export { head } from "./src/element";
 export { a, AnchorContent } from "./src/element";
 export { link } from "./src/element";
