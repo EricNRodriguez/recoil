a5805f735e450a468eca64c72864c1a471ae50b7
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-06 12:18:08 +1000

    update radio dsl helper to use dom data binding api

--- packages/dom-dsl/src/element/input/radio.ts
+++ packages/dom-dsl/src/element/input/radio.ts
@@ -1,7 +1,7 @@
 import { createComponent, IComponentContext } from "../../../../dom-component";
 import { WElement } from "../../../../dom";
 import { Runnable, Supplier } from "../../../../util";
-import { IAtom } from "../../../../atom";
+import {deriveState, IAtom} from "../../../../atom";
 
 export type RadioButtonArguments = {
   isChecked: IAtom<boolean>;
@@ -12,23 +12,9 @@ export const radioButton = createComponent(
     ctx: IComponentContext,
     args: RadioButtonArguments
   ): WElement<HTMLInputElement> => {
-    const radioButtonElement: WElement<HTMLInputElement> = new WElement(
-      document.createElement("input")
-    ).setAttribute("type", "radio");
-
-    // binding effect for checked attribute
-    ctx.runEffect((): void => {
-      const isChecked = args.isChecked.get();
-
-      // changes triggered inside an event handler will be reverted
-      // if preventDefault is called
-      setTimeout(() => {
-        radioButtonElement.unwrap().checked = isChecked;
-      }, 0);
-    });
-
-    // State changes should be reactive
-    radioButtonElement.unwrap().onclick = (e: Event) => e.preventDefault();
+    const radioButtonElement: WElement<HTMLInputElement> = new WElement(document.createElement("input"))
+      .setAttribute("type", "radio")
+      .setProperty("checked", deriveState(() => args.isChecked.get()));
 
     return radioButtonElement;
   }
