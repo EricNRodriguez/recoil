5ebe46be898e6d40fc77091b4b90e35501317482
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-06 15:00:17 +1000

    shift component imports to top of index file

--- packages/dom-dsl/index.ts
+++ packages/dom-dsl/index.ts
@@ -1,3 +1,9 @@
+export {
+  createComponent,
+  StatefulDomBuilder,
+  DomBuilder,
+} from "./src/component/component_factory";
+export { IComponentContext } from "./src/component/component_context";
 export { foreach } from "./src/control/foreach";
 export { ifElse } from "./src/control/if";
 export { match } from "./src/control/match";
@@ -22,9 +28,3 @@ export { label, LabelContent } from "./src/element";
 export { input } from "./src/element";
 export { frag } from "./src/element";
 export { suspense } from "./src/control/suspense";
-export {
-  createComponent,
-  StatefulDomBuilder,
-  DomBuilder,
-} from "./src/component/component_factory";
-export { IComponentContext } from "./src/component/component_context";
