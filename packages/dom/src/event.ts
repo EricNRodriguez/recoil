a9740961e988ed689a42facdd46b57745a937e3a
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-07 11:32:32 +1000

    handle shadow dom/composed events properly

--- packages/dom/src/event.ts
+++ packages/dom/src/event.ts
@@ -4,8 +4,8 @@ import { WNode } from "./node";
 type EventHandlerRef = {event: keyof HTMLElementEventMap, handler: Consumer<any>};
 
 export class GlobalEventCoordinator {
-  private readonly eventTargets: Map<string, WeakSet<Node>> = new Map();
-  private readonly targetHandlers: WeakMap<Node, EventHandlerRef[]> =
+  private readonly eventTargets: Map<string, WeakSet<EventTarget>> = new Map();
+  private readonly targetHandlers: WeakMap<EventTarget, EventHandlerRef[]> =
     new WeakMap();
 
   // the idea here is to decide if we should delegate or if we should attach directly
@@ -14,21 +14,19 @@ export class GlobalEventCoordinator {
   // etc etc. Read into it. As for now I treat every event as composable, which is wrong.
   public attachEventHandler<K extends keyof HTMLElementEventMap>(
     event: K,
-    node: WNode<Node>,
+    target: EventTarget,
     handler: Consumer<HTMLElementEventMap[K]>
   ): void {
     if (!this.eventTargets.has(event)) {
-      // TODO(ericr): provide a way to detach the event handler. Not sure if this should be done here or in the dom wrapper
-      // itself
       this.eventTargets.set(event, new WeakSet());
       document.addEventListener(event, this.executeHandlersBottomUp<K>);
     }
 
-    this.eventTargets.get(event)!.add(node.unwrap());
-    if (!this.targetHandlers.has(node.unwrap())) {
-      this.targetHandlers.set(node.unwrap(), []);
+    this.eventTargets.get(event)!.add(target);
+    if (!this.targetHandlers.has(target)) {
+      this.targetHandlers.set(target, []);
     }
-    this.targetHandlers.get(node.unwrap())!.push({
+    this.targetHandlers.get(target)!.push({
       event: event,
       handler: handler,
     });
@@ -38,35 +36,46 @@ export class GlobalEventCoordinator {
 
   public detachEventHandlers<K extends keyof HTMLElementEventMap>(
     event: K,
-    node: WNode<Node>,
+    target: EventTarget,
   ): void {
-    this.eventTargets.get(event)?.delete(node.unwrap());
+    this.eventTargets.get(event)?.delete(target);
     this.targetHandlers.set(
-      node.unwrap(),
-      this.targetHandlers.get(node.unwrap())?.filter(r => r.event === event) ?? [],
+      target,
+      this.targetHandlers.get(target)?.filter(r => r.event === event) ?? [],
     );
   }
 
-  // TODO(ericr): test this properly.... read the mdn docs properly.... I hacked this together
-  // without much thought. Subtle things like shadow dom boundaries, multiple events on the same node,
-  // events on the document etc etc need to be handled.
+  // simulates the event bubbling phase
   private executeHandlersBottomUp = <K extends keyof HTMLElementEventMap>(
     event: HTMLElementEventMap[K]
   ): void => {
-    const path: EventTarget[] = event.composedPath();
-    let curNode: number = 0;
+    if (!event.bubbles) {
+      throw new Error("delegated events should only be those that bubble");
+    }
 
-    // simulate bubbling targets
-    Object.defineProperty(event, "target", { value: path[0] });
-    Object.defineProperty(event, "currentTarget", { get: () => path[curNode] });
 
-    // bubble bottom-up
-    for (; curNode < path.length && !event.cancelBubble; ++curNode) {
-      const node = path[curNode] as Node;
-      if (this.eventTargets.get(event.type)?.has(node)) {
-        this.targetHandlers.get(node)?.forEach((h) => {
+    let curTarget: EventTarget | null = event.target;
+
+    Object.defineProperty(event, "target", { value: curTarget });
+    Object.defineProperty(event, "currentTarget", { get: () => curTarget });
+
+    // bubble bottom-up - we are traversing the ll manually due to the composedPath
+    // not always containing the complete list of nodes (specifically, closed shadow dom nodes)
+    while (notNullOrUndefined(curTarget) && !event.cancelBubble) {
+      if (this.eventTargets.get(event.type)?.has(curTarget!) ?? false) {
+        this.targetHandlers.get(curTarget!)?.forEach((h) => {
           h.event === event.type && h.handler(event);
-        });
+        })
+      }
+
+      // https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/host
+      if (notNullOrUndefined((curTarget as any)?.host) && (curTarget as any).host instanceof Node) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Event/composed
+        curTarget = event.composed ? (curTarget as any).host : null;
+        // since we have crossed a shadow dom boundary, we need to reset target to the shadow dom host node
+        Object.defineProperty(event, "target", { value: curTarget });
+      } else {
+        curTarget = (curTarget as any).parentNode;
       }
     }
 
