6b383f53ec2798bce3e8ba70e8b1faea7b351fe8
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-11 19:38:17 +1000

    fork current context in foreach

--- packages/dom-dsl/src/control/foreach.ts
+++ packages/dom-dsl/src/control/foreach.ts
@@ -1,9 +1,10 @@
-import {frag, string} from "../element";
+import {b, frag, string} from "../element";
 import { MaybeNodeOrVNode } from "../element";
 import { WNode } from "../../../dom";
 import { IComponentContext, createComponent } from "../../index";
 import { Supplier, Function, notNullOrUndefined } from "../../../util";
 import { wrapInVNode } from "../../../dom/src/node";
+import {lazyChild} from "../component/component_factory";
 
 
 // key value pair used for efficient indexing of existing built elements
@@ -19,6 +20,7 @@ export const foreach = createComponent(
     getItems: Supplier<IndexedItem<T>[]>,
     buildElement: Function<T, WNode<Node>>
   ): WNode<Node> => {
+    buildElement = lazyChild(buildElement);
     const anchor = frag();
 
     let currentItemIndex: Map<string, MaybeNodeOrVNode> = new Map();
