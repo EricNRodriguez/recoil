a74701bb52a3e0c37195e845de2b1c9f5c54dc7c
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-18 20:11:28 +1000

    rename when -> condition

--- packages/dom-jsx/src/control.ts
+++ packages/dom-jsx/src/control.ts
@@ -18,37 +18,34 @@ export const For = createComponent(<T>(ctx: IComponentContext, props: ForProps<T
   return forEach<T>(props);
 });
 
+const mintedTrueComponents: WeakSet<WNode<Node>> = new WeakSet();
 export const True = createComponent((ctx: IComponentContext, props: {}, ...children: WNode<Node>[]): WNode<Node> => {
   const node = frag(...children);
-
-  (node as any).$$$recoilIsTrueComponent = true;
-
+  mintedTrueComponents.add(node);
   return node;
 });
 
 
+const mintedFalseComponents: WeakSet<WNode<Node>> = new WeakSet();
 export const False = createComponent((ctx: IComponentContext, props: {}, ...children: WNode<Node>[]): WNode<Node> => {
   const node = frag(...children);
-
-  (node as any).$$$recoilIsFalseComponent = true;
-
+  mintedFalseComponents.add(node);
   return node;
 });
 
 export type IfProps = {
-    when: boolean | IAtom<boolean>;
+    condition: boolean | IAtom<boolean>;
 };
 
 export const If = createComponent((ctx: IComponentContext, props: IfProps, ...children: WNode<Node>[]): WNode<Node> => {
   const trueChildren = children
-    .filter((c) => (c as any).$$$recoilIsTrueComponent);
+    .filter(mintedTrueComponents.has.bind(mintedTrueComponents));
 
   const falseChildren = children
-    .filter((c) => (c as any).$$$recoilIsFalseComponent);
-
+    .filter(mintedFalseComponents.has.bind(mintedFalseComponents));
 
   return ifElse({
-    condition: props.when,
+    condition: props.condition,
     ifTrue: () => frag(...trueChildren),
     ifFalse: () => frag(...falseChildren),
   });
