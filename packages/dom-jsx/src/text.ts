489326d10aa70f5c7ef908b032cdfb9a4fd8a91c
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-27 23:04:01 +1000

    apply a non-caching transform instead of wrapping in a derivedstate

--- packages/dom-jsx/src/text.ts
+++ packages/dom-jsx/src/text.ts
@@ -6,9 +6,7 @@ export type TextNodeTypes = string | boolean | number;
 export type TextNodeSource = TextNodeTypes | Supplier<TextNodeTypes> | IAtom<TextNodeTypes>;
 export const $ = (data: TextNodeSource): WNode<Node> => {
     if (isAtom(data)) {
-        // TODO(ericr): introduce the concept of a "atomic mutation" to atoms to avoid having another
-        // wrapper atom
-        return createTextNode(deriveState(() => (data as IAtom<TextNodeTypes>).get().toString()));
+        return createTextNode((data as IAtom<TextNodeTypes>).transform((v: TextNodeTypes) => v.toString()));
     } else if (typeof data === "function") {
         return createTextNode(deriveState(() => (data as Supplier<TextNodeTypes>)().toString()));
     } else {
