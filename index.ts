7db6007f7d7f42794c8791d3f774c3a863c90e0b
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-08-28 15:02:37 +1000

    add main entrypoint to package json

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "packages/atom/index";
+export * from "packages/dom/index";
+export * from "packages/context/index";
+export * from "packages/dom-dsl/index";
+export * from "packages/dom-jsx/index";
+export * from "packages/lazy/index";
