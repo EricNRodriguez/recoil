fd0c2b072a61f906d3b9564e9bf47cd263d37bc6
Eric Rodriguez <eric.rodriguez@Erics-MacBook-Pro.local>
2022-09-04 14:12:13 +1000

    added Node to module resolution

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import typescript from '@rollup/plugin-typescript'
+import pkg from './package.json'
+
+export default [
+    {
+        input: 'dist/index.js',
+        output: {
+            name: 'atom',
+            file: `dist/${pkg.main}`,
+            format: 'umd',
+            globals: {
+                // rxjs: 'rxjs',
+                // 'rxjs/operators': 'rxjs/operators'
+            },
+            sourcemap: true
+        },
+        external: [
+            // 'rxjs',
+            // 'rxjs/operators'
+        ]
+    },
+    {
+        input: 'index.ts',
+        output: [
+            { file: `dist/${pkg.module}`, format: 'es', sourcemap: true },
+            { file: `dist/${pkg.commonJs}`, format: 'cjs', sourcemap: true }
+        ],
+        external: [
+            //     'rxjs',
+            //     'rxjs/operators'
+        ],
+        plugins: [typescript({ tsconfig: 'tsconfig.build.json' })]
+    }]
