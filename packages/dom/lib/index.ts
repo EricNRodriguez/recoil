abb086defe5c9399a7bf1c7b81cd107db2fa04df
eric <eric@g.com>
2022-09-10 12:02:35 +1000

    rename factory (file) to api

--- packages/dom/lib/index.ts
+++ packages/dom/lib/index.ts
@@ -1,3 +1,3 @@
 export { WNode, isWNode } from "./node";
 export { WElement } from "./element";
-export { createElement, createFragment, createTextNode } from "./factory";
+export { createElement, createFragment, createTextNode } from "./api";
