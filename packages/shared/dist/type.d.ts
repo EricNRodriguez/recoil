e210dd3ac3f10e73761e8a76d03e34702d0cf239
eric <eric@g.com>
2022-09-10 17:58:19 +1000

    add F and FDecorator primitive types to shared

--- packages/shared/dist/type.d.ts
+++ packages/shared/dist/type.d.ts
@@ -1,2 +1,4 @@
 export declare type NonEmpty<T> = [T, ...T[]];
+export declare type F<Args extends unknown[], ReturnType> = (...args: [...Args]) => ReturnType;
+export declare type FDecorator<Args extends unknown[], ReturnType> = F<[F<Args, ReturnType>], F<Args, ReturnType>>;
 //# sourceMappingURL=type.d.ts.map
--- packages/shared/lib/type.ts
+++ packages/shared/lib/type.ts
@@ -1,1 +1,3 @@
 export type NonEmpty<T> = [T, ...T[]];
+export type F<Args extends unknown[], ReturnType> = (...args: [...Args]) => ReturnType;
+export type FDecorator<Args extends unknown[], ReturnType> = F<[F<Args, ReturnType>], F<Args, ReturnType>>;
