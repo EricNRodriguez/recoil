1a95d1942e52bbab46cc93f2ec24c32cca721b5d
eric <eric@g.com>
2022-09-11 13:16:01 +1000

    correctly leak the root node wrapper - v8s gc is smarter than anticipated

--- packages/dom-dsl/lib/run_app.ts
+++ packages/dom-dsl/lib/run_app.ts
@@ -1,8 +1,7 @@
 import { WNode, wrapElement } from "recoiljs-dom";
 
-const mountedApps: WNode<Node>[] = [];
 export const runApp = (anchor: HTMLElement, app: WNode<Node>): void => {
-  wrapElement(anchor).setChildren([app]);
-  // prevent gc
-  mountedApps.push(app);
+  wrapElement(anchor)
+    .bindScopeToWrappedNode()
+    .setChildren([app]);
 };
